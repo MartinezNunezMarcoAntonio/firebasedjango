9a5b7649a29f0f96cfc8e1361ea14522e7a5399e
antonio <marcoantonio_martinez@outlook.com>
2020-09-24 06:14:06 -0500

    enlace con firebase y cambios del git ignore

--- firebasedjango/urls.py
+++ firebasedjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from postres.views import Postres
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('postres/' ,Postres.as_view() ,name="index"),
 ]
--- postres/views.py
+++ postres/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+import firebase_admin
+
+from firebase_admin import credentials
+from firebase_admin import db
+
+class Postres( View ):
+    template_name = 'index.html'
+    cred = credentials.Certificate('./fir-django-edfdb-firebase-adminsdk-nqz49-77a1d24e82.json')
+    firebase_admin.initialize_app(cred,
+        {'databaseURL': 'https://fir-django-edfdb.firebaseio.com'}
+    )
+
+    ref = db.reference('postres')
+
+    datos = ref.get()
+
+    def get(self ,request):
+        return render(request ,self.template_name , {"productos":self.datos})
+
